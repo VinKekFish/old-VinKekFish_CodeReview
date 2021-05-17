ae7aae66d9ac0ba9be8e6f606f2d1c505f4276c5
fdsc <fdsc@yandex.ru>
2021-05-17 15:59:45 +0300

    Тесты тормозов BytesBuilderForPointers Возникают, кажется, при очистке

--- _Tests/BytesBuilder/Test/20210511-keccakprng/Program.cs
+++ _Tests/BytesBuilder/Test/20210511-keccakprng/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using vinkekfish.keccak.keccak_20200918;
+
 namespace _20210511_keccakprng
 {
     unsafe class Program
@@ -21,15 +23,55 @@ namespace _20210511_keccakprng
                 prng.InputKeyAndStep(_b0, b0.Length, null, 0);
             }
 
-            // 720 тыс.
+            var table1 = new ushort[3200];
+            for (ushort i = 0; i < table1.Length; i++)
+            {
+                table1[i] = i;
+            }
+
             var dt1 = DateTime.Now;
             for (var i = 0; i < count; i++)
             {
                 prng.calcStep(inputReadyCheck: prng.isInputReady, SaveBytes: true);
+                prng.output.Clear();
             }
             var dt2 = DateTime.Now;
             var ms  = (dt2-dt1).TotalMilliseconds;
+            Console.WriteLine("Блоков в секунду: " + (count * 1000.0 / ms));
+
+
+            const int countForPermutations = count * 8 / 3200;
+
+            dt1 = DateTime.Now;
+            // 8 чисел на один блок (64/8), 3200 чисел на таблицу
+            using (var b8 = new cryptoprime.BytesBuilderForPointers.AllocHGlobal_AllocatorForUnsafeMemory().AllocMemory(8))
+            for (var i = 0; i < countForPermutations; i++)
+            {
+                //prng.calcStep(inputReadyCheck: prng.isInputReady, SaveBytes: true);
+                // prng.doRandomPermutationForUShorts(table1);
+
+                var len = (ulong) table1.LongLength;
+                for (ulong j = 0; j < len - 1; j++)
+                {
+                    // Keccak_PRNG_20201128.getCutoffForUnsignedInteger(0, (ulong) len - j - 1, out ulong cutoff, out ulong range);
+                    Keccak_PRNG_20201128.getCutoffForUnsignedInteger(0, 3, out ulong cutoff, out ulong range);
+                    prng.getUnsignedInteger(0, cutoff, range, b8);
+                }
+            }
+            dt2 = DateTime.Now;
+            ms  = (dt2-dt1).TotalMilliseconds;
             Console.WriteLine("Операций в секунду: " + (count * 1000.0 / ms));
+
+            dt1 = DateTime.Now;
+            // 8 чисел на один блок (64/8), 3200 чисел на таблицу
+            for (var i = 0; i < countForPermutations; i++)
+            {
+                prng.doRandomPermutationForUShorts(table1);
+            }
+            dt2 = DateTime.Now;
+            ms  = (dt2-dt1).TotalMilliseconds;
+            // Вывод всё равно в блоках: значения приведены в эквивалентную величину
+            Console.WriteLine("Блоков в секунду: " + (count * 1000.0 / ms));
 
             prng.Dispose();
 
---
+++ _Tests/BytesBuilder/Test/LightRandomGenerator_test/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LightRandomGenerator_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // const int count = 65536;
+            // const int size  = 4096;
+
+            var b0 = new byte[4] {0, 0, 0, 0};
+
+            var prng = new vinkekfish.VinKekFish_k1_base_20210419_keyGeneration();
+
+            prng.EnterToBackgroundCycle();
+            Thread.Sleep(1000);
+            prng.ExitFromBackgroundCycle();
+                /*
+            // 720 тыс.
+            var dt1 = DateTime.Now;
+            for (var i = 0; i < count; i++)
+            {
+                
+            }
+            var dt2 = DateTime.Now;
+            var ms  = (dt2-dt1).TotalMilliseconds;
+            Console.WriteLine("Операций в секунду: " + (count * 1000.0 / ms));
+            */
+
+            Console.WriteLine("Блоков в секунду: " + prng.BackgourndGenerated);
+
+            prng.Dispose();
+
+            Console.ReadKey();
+        }
+    }
+}
